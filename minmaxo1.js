c0afb8fe0ae8a0983479b38d44b3cc4f3cba65f0
Jared Parker <jared.parker@gmail.com>
2025-03-09 15:16:07 -0700

    midway doing this problem

---
+++ minmaxo1.js
@@ -0,0 +1,134 @@
+console.log('minmaxo1');
+
+class ListNode {
+    constructor(prev, next, val){
+        this.next = next;
+        this.prev = prev;
+        this.keys = new Set();
+        this.val = val;
+    }
+}
+
+class minmax {
+    constructor(){
+        this.keyMap = new Map();
+        this.frequencyMap = new Map();
+        this.root = new ListNode(null, null, 0);
+        this.root.next = this.root;
+        this.root.prev = this.root;
+        this.frequencyMap.set(0, this.root);
+    }
+
+    removeNode(frequency) {
+        if(frequency === 0){
+            return;
+        } else {
+            const node = this.frequencyMap.get(frequency);
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+        }
+    }
+
+    addNodeUp(prevNode, frequency) {
+        const newNode = new ListNode(prevNode, prevNode.next, frequency);
+        prevNode.next.prev = newNode;
+        prevNode.next = newNode;
+        this.frequencyMap.set(frequency, newNode);
+        return newNode;
+    }
+
+    addNodeDown(nextNode, frequency) {
+        const newNode = new ListNode(nextNode.prev, nextNode, frequency);
+        nextNode.prev.next = newNode;
+        nextNode.prev = newNode;
+        this.frequencyMap.set(frequency, newNode);
+        return newNode;
+    }
+
+    removeNode(frequency) {
+        const node = this.frequencyMap.get(frequency);
+        let prev = node.prev;
+        let next = node.next;
+
+        prev.next = next;
+        next.prev = prev;
+        this.frequencyMap.delete(frequency);
+    }
+
+    put(key) {
+        let frequency = this.keyMap.get(key);
+        if(frequency == null){
+            frequency = 0;
+        }
+        frequency++;
+        this.keyMap.set(key, frequency);
+
+        let previousNode = this.frequencyMap.get(frequency - 1);
+        let node = this.frequencyMap.get(frequency);
+
+        if(node == null){
+            console.log('creating ' + frequency);
+            node = this.addNodeUp(previousNode, frequency);
+        }
+        
+        previousNode.keys.delete(key);
+        if(previousNode.keys.size === 0 && previousNode.val !== 0){
+            console.log('removing ' + previousNode.val);
+            this.removeNode(previousNode.val);
+        }
+
+        node.keys.add(key);
+    }
+
+    remove(key){
+        let frequency = this.keyMap.get(key);
+        const oldNode = this.frequencyMap.get(frequency);
+        frequency--;
+        let node = this.frequencyMap.get(frequency);
+
+        if(node == null){
+            node = this.addNodeDown(oldNode, frequency);
+        }
+
+        if(frequency === 0){
+            this.keyMap.delete(key);
+        } else {
+            this.keyMap.set(key, frequency);
+            node.keys.add(key);
+        }
+
+        oldNode.keys.delete(key);
+        if(oldNode.keys.size === 0){
+            this.removeNode(oldNode.val);
+        }
+    }
+
+    getMin(){
+        return this.root.val;
+    }
+
+    getMax(){
+        let tail = this.root.prev;
+        return tail.val;
+    }
+}
+
+
+const testObject = new minmax();
+
+testObject.put('foo');
+testObject.put('foo');
+testObject.put('foo');
+testObject.remove('foo');
+testObject.remove('foo');
+testObject.put('bar');
+testObject.put('bar');
+testObject.put('testing');
+testObject.put('testing');
+testObject.put('testing');
+testObject.put('bar');
+
+console.log(testObject.root);
+
+console.log(testObject.getMin());
+console.log(testObject.getMax());
